5708ba933584d86a71700e07ceda95b96f30a3b8
ibrahim <iadwan_89@hotmail.com>
2026-09-04 20:47:01 +0300

    updating world

--- assets/Objects/player.gd
+++ assets/Objects/player.gd
@@ -1,11 +1,22 @@
 extends CharacterBody2D
 
+@onready var ladder_ray: RayCast2D = $RayCast2D
 
-const SPEED = 50.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -350.0
+const LADDER_SPEED = 70.0
 
 
 func _physics_process(delta: float) -> void:
+	var on_ladder := ladder_ray.is_colliding()
+	var vertical_input := Input.get_axis("ui_up", "ui_down")
+
+	if on_ladder and vertical_input != 0:
+		velocity.x = 0
+		velocity.y = vertical_input * LADDER_SPEED
+		move_and_slide()
+		return
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -15,7 +26,6 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
